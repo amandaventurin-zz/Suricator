00ffce48b26c9384799a1762075aaca4a373f122
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-10-15 16:03:12 -0300

    API do Genius implementada e funcionando

---
+++ backend/src/lyric.ts
@@ -0,0 +1,11 @@
+import Lyricist from "lyricist";
+
+const lyricist = new Lyricist(
+	"RDH4LAPsVqv2BIl2SMge1vx3smzx0p6IZSHBeJZWXv7EO5wMOcP0ont0LiaJ3osO"
+);
+
+export async function search(string: string) {
+	const hits = await lyricist.search(string);
+	const song = await lyricist.song(hits[0].id, { fetchLyrics: true });
+	return song.lyrics;
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,7 @@
 import express, { response } from "express";
-import { PythonShell } from "python-shell";
-import request from "request-promise";
-import fetch from "node-fetch";
+import {search} from"./lyric"
 
 const app = express();
-
-const apiURL = "https://api.lyrics.ovh";
-
-async function searchSongs(searchValue: string) {
-	const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
-	const data = await searchResult.json();
-	return data;
-}
-
-async function getLyrics(data: any) {
-    let lyric =""
-    let i = 0
-    // while (lyric == "") {
-    //     const res = await fetch(
-	// 		`${apiURL}/v1/${data[i].artist.name}/${data[i].title_short}`
-    //     );
-    //     const aux = await res.json();
-    //     lyric = aux.lyrics;
-    //     i++
-    // }
-	
-	
-	// lyric = lyric.replace(/\\n\\n\\n/i, " ");
-	// lyric = lyric.replace(/\\n\\n/i, " ");
-	// lyric = lyric.replace(/\\r\\n/i, " ");
-	// lyric = lyric.replace("\r", "");
-	// lyric = lyric.replace("\n", "");
-	return data;
-}
 
 app.get("/", async (request, response) => {
 	// const pythonShell = new PythonShell("main.py", {
@@ -43,10 +12,20 @@ app.get("/", async (request, response) => {
 	// });
 
 	// pythonShell.on("message", function (message) {
-	// 	console.log(message);
-    const data = await searchSongs("back in black");
-	const lyric = await getLyrics(data.data);
-	return response.json(lyric);
+    // 	console.log(message);
+    
+    let lyrics = await search("the trooper iron maiden")
+
+    lyrics = lyrics.replace(/\[Verse\s\d\]\n/g,"")
+    lyrics = lyrics.replace(/\[Verse\s\d\]/g,"")
+    lyrics = lyrics.replace(/\[Refrain\]/g,"")
+    lyrics = lyrics.replace(/\[Guitar solo\]/g,"")
+    lyrics = lyrics.replace(/\n\n\n/g,".")
+    lyrics = lyrics.replace(/\n\n/g,".")
+    lyrics = lyrics.replace(/\n/g, ".")
+
+    lyrics = lyrics.trim()
+	return response.json(lyrics);
 });
 
 app.listen(3333);
