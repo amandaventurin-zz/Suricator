bf18469d40cfcca9d744607a26a3585114f16cc4
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-10-15 12:33:09 -0300

    get suggested lyric

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,42 +1,43 @@
 import express, { response } from "express";
 import { PythonShell } from "python-shell";
-import request from "request";
+import request from "request-promise";
 import fetch from "node-fetch";
 
 const app = express();
 
+let songs: any;
 let song: any;
 let lyric: any;
 
-async function geral() {
-	const songs: any = await fetch(
-		`https://api.lyrics.ovh/suggest/${"three little birds"}`
-	);
+// async function geral() {
+// 	songs = await fetch(
+// 		`https://api.lyrics.ovh/suggest/${"three little birds"}`
+// 	);
+//     await console.log(songs)
+// 	let song = await songs.json();
+// 	song = await song.data[1];
+// 	// console.log(song)
+// 	let lyric = await fetch(
+// 		`https://api.lyrics.ovh/v1/${song.artist.name}/${song.title}`
+// 	);
+// 	// await console.log(lyric.json());
 
-	let song = await songs.json();
-	song = await song.data[1];
-	// console.log(song)
-	let lyric = await fetch(
-		`https://api.lyrics.ovh/v1/${song.artist.name}/${song.title}`
-	);
-	// await console.log(lyric.json());
+// 	lyric = await lyric.json();
 
-	lyric = await lyric.json();
+//     let lyric_string = await lyric.lyrics;
 
-    let lyric_string = await lyric.lyrics;
-    
-	lyric_string = lyric_string.replace(/\r\n/g, " ");
-	lyric_string = lyric_string.replace(/\r/g, " ");
-	lyric_string = lyric_string.replace(/\n\n\n\n/g, " ");
-	lyric_string = lyric_string.replace(/\n\n\n/g, " ");
-    lyric_string = lyric_string.replace(/\n/g, " ");
-    
-	// console.log("Lyric");
-	console.log(lyric_string);
-	return lyric_string;
-}
+// 	lyric_string = lyric_string.replace(/\r\n/g, " ");
+// 	lyric_string = lyric_string.replace(/\r/g, " ");
+// 	lyric_string = lyric_string.replace(/\n\n\n\n/g, " ");
+// 	lyric_string = lyric_string.replace(/\n\n\n/g, " ");
+//     lyric_string = lyric_string.replace(/\n/g, " ");
 
-geral();
+// 	// console.log("Lyric");
+// 	// console.log(lyric_string);
+// 	return lyric_string;
+// }
+
+// geral();
 
 // const func2 = request(
 // 	`https://api.lyrics.ovh/v1/${song.artist.name}/${song.title}`,
@@ -52,6 +53,50 @@ geral();
 // 		console.log(lyric);
 // 	}
 // );
+const apiURL = "https://api.lyrics.ovh";
+
+async function searchSong(searchValue: string) {
+	const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
+	const data = await searchResult.json();
+
+	// console.log(data.data[0]);
+	// showData(data);
+	return data;
+}
+
+// interface Data{
+//     id: number;
+//     title_short: string;
+//     rank: number;
+//     artist_name: string;
+// }
+
+async function getLyrics(data: any) {
+    let lyric =""
+    
+    let i = 0
+    while (lyric == "") {
+        const res = await fetch(
+			`${apiURL}/v1/${data[i].artist.name}/${data[i].title_short}`
+        );
+        const aux = await res.json();
+        lyric = aux.lyrics;
+        i++
+    }
+	
+	
+	lyric = lyric.replace("\n", "");
+	lyric = lyric.replace("\r", "");
+	lyric = lyric.replace(/\\r\\n/i, " ");
+	lyric = lyric.replace(/\\n\\n/i, " ");
+	lyric = lyric.replace(/\\n\\n\\n/i, " ");
+	return lyric;
+
+	// const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+	// result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+	// <p>${lyrics}</p>`;
+}
 
 app.get("/", async (request, response) => {
 	// const pythonShell = new PythonShell("main.py", {
@@ -63,10 +108,9 @@ app.get("/", async (request, response) => {
 
 	// pythonShell.on("message", function (message) {
 	// 	console.log(message);
-	// });
-	geral();
-
-	return response.json(response);
+    const data = await searchSong("viva la vida coldplay");
+	const lyric = await getLyrics(data.data);
+	return response.json(lyric);
 });
 
 app.listen(3333);
